bc1ab162056c9fa34f0eb8fc26704ebb27c8463a
AnimeBubsi <95187459+AnimeBubsi@users.noreply.github.com>
2022-04-30 16:44:08 +0300

    Update config.lua

--- esx_DrugRobbery/config.lua
+++ esx_DrugRobbery/config.lua
@@ -5,11 +5,21 @@ Config.treppu = false -- true if need bag false if dont need bag
 
 Config.reppu = {40, 41, 44, 45}
 
-Config.RequiredCopsRob = 6
+Config.RequiredCopsRob = 0
 
 Config.MinHuume = 2 -- min drug
 
 Config.MaxHuume = 6 -- Max drug
+
+Config.tavara1 = 'coke' -- Item 1
+Config.tavara2 = 'amfe_pooch' -- Item 2
+Config.tavara3 = 'amfe' -- Item 3
+Config.tavara4 = 'cannabis' -- Item 4
+Config.tavara5 = 'marijuana' -- Item 5
+Config.tavara6 = 'coke_pooch' -- Item 6
+Config.tavara7 = 'Opium' -- Item 7
+
+Config.tyo = 'police' -- Job who get an alarm
 
 Config.Maxlaatikko = 25 -- dont touch :D
 
@@ -24,3 +34,10 @@ Stores = {
 		lastrobbed = 0
 	}
 }
+
+
+
+
+
+
+-- Sry for bad english i hope you understand my dog water english - Bubsi :D
