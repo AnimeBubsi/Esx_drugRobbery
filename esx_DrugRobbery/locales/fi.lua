154c6c19a1e42b130839800c5edd4ea3d2bf9d0e
AnimeBubsi <95187459+AnimeBubsi@users.noreply.github.com>
2022-06-12 23:19:55 +0300

    Update fi.lua

--- esx_DrugRobbery/locales/fi.lua
+++ esx_DrugRobbery/locales/fi.lua
@@ -1,29 +1,32 @@
 Locales['fi'] = {
 
-	['robbery_cancelled'] = 'Ryöstö keskeytyi!',
+	['ryosto_cancelled'] = 'Ryöstö keskeytyi!',
 	['shop_ryosto'] = 'HuumeRyöstö',
 	['press_to_rob'] = 'Ammu ilmaan aloittaaksesi ryöstön',
 	['seconds_remaining'] = '~w~ sekunttia jäljellä',
-	['robbery_cancelled_at'] = '~r~ ryöstö keskeytyi kohteessa: ~b~',
-	['robbery_has_cancelled'] = '~r~ Ryöstö keskeytyi: ~b~',
+	['ryosto_cancelled_at'] = '~r~ ryöstö keskeytyi kohteessa: ~b~',
+	['ryosto_has_cancelled'] = '~r~ Ryöstö keskeytyi: ~b~',
 	['already_robbed'] = 'Huumet on jo varastettu. odota: ',
 	['seconds'] = 'Sekunttia.',
 	['rob_in_prog'] = '~r~ Ryöstö käynissä kohteessa: ~b~',
 	['started_to_rob'] = 'Sinä aloitit ryöstön',
 	['do_not_move'] = ', Ota huumeet pöydiltä',
 	['hold_pos'] = 'Etsi Kaikki paikat saarelta!',
-	['robbery_complete'] = '~r~ Ryöstö onnistui!~s~',
-	['robbery_complete_at'] = '~r~ Ryöstö onnistui kohteessa: ~b~',
+	['ryosto_complete'] = '~r~ Ryöstö onnistui!~s~',
+	['ryosto_complete_at'] = '~r~ Ryöstö onnistui kohteessa: ~b~',
 	['min_two_police'] = 'Kaupungissa pitää olla ~b~ poliisia jotta voit ryöstää huumeet',
 	['min_two_police2'] = ' ~w~Poliisia että voi ryöstää.',
-	['robbery_already'] = '~r~Ryöstö käynissä paikassa.',
-	['robbery_has_ended'] = 'Ryöstö onnistui',
+	['ryosto_already'] = '~r~Ryöstö käynissä paikassa.',
+	['ryosto_has_ended'] = 'Ryöstö onnistui',
 	['end'] = 'Saaren huumepaikat on ryöstetty',
 	['field'] = 'Paina ~y~E~s~ ~o~Kerätäksesi~s~ huumeita',
 	['collectinprogress'] = 'Keräät huumeita',
 	['press_to_collect'] = 'Kerätäksesi huumeet',
 	['tlaatikko'] = 'Tutkitut paikat',
 	['need_bag'] = 'Tarvitset repun',
-        ['Poliisi'] = 'Henkilöt voivat ovat jossain saaralle'
-
+        ['Poliisi'] = 'Henkilöt voivat ovat jossain saaralle',
+        ['lopetat'] = 'Lopetat ryöstöä',
+        ['lopetit'] = 'Lopetit ryöstön',
+        ['etryosta'] = 'Et ole ryöstämässä huumeita'
 }
+
