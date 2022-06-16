63e3b3df31954319171b20ec6d061661cb87de2e
AnimeBubsi <95187459+AnimeBubsi@users.noreply.github.com>
2022-06-17 00:47:26 +0300

    Update sv_esx_drugheist.lua

--- esx_DrugRobbery/server/sv_esx_drugheist.lua
+++ esx_DrugRobbery/server/sv_esx_drugheist.lua
@@ -48,6 +48,7 @@ AddEventHandler('esx_drugheist_ryosto:endrob', function(robb)
 		TriggerClientEvent('esx_drugheist_ryosto:ryostocomplete', source)
 		robbers[source] = nil
 		TriggerClientEvent('esx:showNotification', source, _U('ryosto_has_ended') .. Stores[robb].nimi)
+                Dclogi(122000, "DrugRobbery", nimi.. " has robbed the drugs")
 	end
 end)
 
@@ -71,15 +72,16 @@ AddEventHandler('esx_drugheist_ryosto:rob', function(robb)
 	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	local xPlayers = ESX.GetPlayers()
+        local nimi = GetPlayerName(source)
 	
 	if Stores[robb] then
 
 		local store = Stores[robb]
 
 		if (os.time() - store.lastrobbed) < Config.SecBetwNextRob and store.lastrobbed ~= 0 then
-
-            TriggerClientEvent('esx_drugheist_ryosto:togliblip', source)
+            
 			TriggerClientEvent('esx:showNotification', source, _U('already_robbed') .. (Config.SecBetwNextRob - (os.time() - store.lastrobbed)) .. _U('seconds'))
+                        Dclogi(122000, "DrugRobbery", nimi.. " Tryed to start drugRobbery but it was already robbed")
 			return
 		end
 
@@ -98,6 +100,7 @@ AddEventHandler('esx_drugheist_ryosto:rob', function(robb)
 			TriggerClientEvent('esx:showNotification', source, _U('started_to_rob') .. store.nimi .. _U('do_not_move'))
 			TriggerClientEvent('esx:showNotification', source, _U('hold_pos'))
 			TriggerClientEvent('esx_drugheist_ryosto:currentlyrobbing', source, robb)
+                        Dclogi(122000, "DrugRobbery", nimi.. " Started DrugRobbery")
             CancelEvent()
 			Stores[robb].lastrobbed = os.time()
 		else
@@ -135,3 +138,18 @@ ESX.RegisterServerCallback('esx_drugheist_ryosto:aloitus', function(source, cb)
 
 	cb(CopsConnected)
 end)
+
+function Dclogi(color, name, message, footer)
+  local footer = 'Bot - '..os.date("%d/%m/%Y - %X")
+  local embed = {
+        {
+            ["color"] = color,
+            ["title"] = "**".. name .."**",
+            ["description"] = message,
+            ["footer"] = {
+            ["text"] = footer,
+            },
+        }
+    }
+  PerformHttpRequest(Config.webhook, function(err, text, headers) end, 'POST', json.encode({username = Config.botname, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
